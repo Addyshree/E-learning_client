5d1bd11ea28224d9e1ab88ed44a357b74c2138d6
Addyshree <144418962+Addyshree@users.noreply.github.com>
2025-07-12 18:03:28 +0530

    Update CourseCard.jsx
    case sensitive coursecard.css

--- src/components/coursecard/CourseCard.jsx
+++ src/components/coursecard/CourseCard.jsx
@@ -1,73 +1,8 @@
-// import React from "react";
-// import "./coursecard.css";
-// import { server } from "../../main";
-// import { UserData } from "../../auth/context/Usercontext";
-// import { useNavigate } from "react-router-dom";
 
-// function CourseCard({ course }) {
-//   const navigate = useNavigate();
-//   const { user, isAuth } = UserData();
-//   return (
-//     <div className="course-card">
-//       <img src={`${server}/${course.image}`} alt="" className="course-image" />
-//       <h3>{course.title}</h3>
-//       <p>Instructor : {course.createdBy}</p>
-//       <p>Duration : {course.duration}</p>
-//       <p>Price : ₹{course.price} </p>
-//       {isAuth ? (
-//         <>
-//           {user && user.role !== "admin" ? (
-//             <>
-//               {user.subscription.includes(course._id) ? (
-//                 <button
-//                   onClick={() => navigate(`/course/study/${course._id}`)}
-//                   className="common-btn"
-//                 >
-//                   Study
-//                 </button>
-//               ) : (
-//                 <button
-//                   onClick={() => navigate(`/course/${course._id}`)}
-//                   className="common-btn"
-//                 >
-//                   Get Started
-//                 </button>
-//               )}
-//             </>
-//           ) : (
-//             <button
-//               onClick={() => navigate(`/course/study/${course._id}`)}
-//               className="common-btn"
-//             >
-//               Study
-//             </button>
-//           )}
-//         </>
-//       ) : (
-//         <button onClick={() => navigate("/login")} className="common-btn">
-//           Get Started
-//         </button>
-//       )}
 
-//       <br />
-
-//       {user && user.role === "admin" && (
-//         <button
-//           onClick={() => deleteHandler(course._id)}
-//           className="common-btn"
-//           style={{ background: "red" }}
-//         >
-//           Delete
-//         </button>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default CourseCard;
 
 import React from "react";
-import "./courseCard.css";
+import "./coursecard.css";
 import { server } from "../../main";
 import { UserData } from "../../context/UserContext";
 import { useNavigate } from "react-router-dom";
