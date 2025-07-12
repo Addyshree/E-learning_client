a17e0320365045904f5a8b3c349690aa39cf8c23
Addyshree <144418962+Addyshree@users.noreply.github.com>
2025-07-12 19:14:18 +0530

    Update main.jsx
    Updating the backend url

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 import { UserContextProvider } from "./context/UserContext.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
 
-export const server = "https://e-learningserver-production-0312.up.railway.app/";
+export const server = "https://e-learningserver-production-0312.up.railway.app";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
